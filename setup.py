b7e31179ea77f3bea0eda781fa93ff01086fca7d
Paul Weaver <paul@ruthorn.co.uk>
2017-09-06 10:50:47 +0100

    Update setup.py to point to new repo location

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     packages=['svgast'],
     decsription='Helpers for building SVG files in Python',
     keywords=['svg', 'xml'],
-    url='http://github.com/ch3pjw/concert_svgast',
+    url='http://github.com/concertdaw/svgast',
     author='Paul Weaver',
-    author_email='paul@ruthorn.co.uk',
+    author_email='paul@concertdaw.co.uk',
     version='0.0.0',
     license='',
     install_requires=[
